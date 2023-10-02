e738f3945e40d2fb2dfbaa9c4a7b93b7cc39545d
Onat Somer <89694072+OnatSoft@users.noreply.github.com>
2023-10-02 08:11:34 +0300

    Update DersEkleForm.cs

--- EtutSistemi15/DersEkleForm.cs
+++ EtutSistemi15/DersEkleForm.cs
@@ -13,7 +13,7 @@ namespace EtutSistemi15
 {
     public partial class DersEkleForm : Form
     {
-        SqlConnection baglan = new SqlConnection(@"Data Source=LAPTOP-ONATSOFT\ONATSOFT;Initial Catalog=EtutSistemiDB;Integrated Security=True");
+        SqlConnection baglan = new SqlConnection(@"");
 
         public DersEkleForm()
         {
